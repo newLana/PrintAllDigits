02a13addb6905d3b31219d5f965c862ab3d0ff03
newLana <newLana@users.noreply.github.com>
2018-11-04 14:41:48 +0200

    Add project files.

---
+++ PrintAllDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace PrintAllDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDigits(122);
+            Console.ReadKey();
+        }
+
+        static void PrintDigits(int num)
+        {            
+            string s = num.ToString();
+            int _num = Int32.Parse(s.Last().ToString());
+            if (s.Length > 1)
+            {               
+                PrintDigits(Int32.Parse(s.Remove(s.Length - 1)));
+            }
+            Console.Write(_num);
+        }
+    }
+}

319bf496e4074e98f3fb83c927b98ad5d5e2a867
newLana <newLana@users.noreply.github.com>
2018-11-06 22:16:12 +0200

    Refactored

--- PrintAllDigits/Program.cs
+++ PrintAllDigits/Program.cs
@@ -7,19 +7,18 @@ namespace PrintAllDigits
     {
         static void Main(string[] args)
         {
-            PrintDigits(122);
+            PrintDigits(12245891);
             Console.ReadKey();
         }
 
         static void PrintDigits(int num)
         {            
             string s = num.ToString();
-            int _num = Int32.Parse(s.Last().ToString());
             if (s.Length > 1)
             {               
                 PrintDigits(Int32.Parse(s.Remove(s.Length - 1)));
             }
-            Console.Write(_num);
+            Console.Write(Int32.Parse(s.Last().ToString()));
         }
     }
 }
